8322f2b988266f5c33c557cc7491fb0716070e8d
luk-rs <santos8@gmail.com>
2024-05-24 21:15:14 +0100

    wrapped up scripts and comments

--- 004-mint-tokens.sh
+++ 004-mint-tokens.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
-# fund wallets from the one funded from faucet
+# mint ref hotel nft
+# args
+#* 1 - token Name
+#* 2 - number of shares
+#* 3 - individual share cost
+#* 4 - stay cost per night
+#* 5 - rental profitability share
 deno run --allow-env --allow-read --allow-net ./execution/001-mint-tx.ts hotelPark 1000 10 100 45
--- 005-buy-shares.sh
+++ 005-buy-shares.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
-# fund wallets from the one funded from faucet
+# mint ref hotel nft
+# args
+#* 1 - token Name
+#* 2 - amount of shares to buy
+#* 3 - wallet to buy (refer to .wallets/pk folder after executing `001-pre-execution-setup.sh` script) [IGNORE THE EXTENSION]
 deno run --allow-env --allow-read --allow-net ./execution/002-buy-shares-tx.ts hotelPark 20 "share_holder_1"
